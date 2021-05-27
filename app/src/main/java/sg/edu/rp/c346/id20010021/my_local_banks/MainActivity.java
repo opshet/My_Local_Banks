cd3fcee1171c6b6a3cd20559839b4ab1dd5223a2
opshet <20010021@myrp.edu.sg>
2021-05-27 15:38:19 +0800

    done

--- app/src/main/java/sg/edu/rp/c346/id20010021/my_local_banks/MainActivity.java
+++ app/src/main/java/sg/edu/rp/c346/id20010021/my_local_banks/MainActivity.java
@@ -3,6 +3,7 @@ package sg.edu.rp.c346.id20010021.my_local_banks;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.ContextMenu;
@@ -12,13 +13,19 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
+import android.widget.ToggleButton;
 
 public class MainActivity extends AppCompatActivity {
     TextView DBS;
     TextView OCBC;
     TextView UOB;
 
+    Boolean FavouriteDBS;
+    Boolean FavouriteOCBC;
+    Boolean FavouriteUOB;
+
     String wordClicked = "";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,6 +49,7 @@ public class MainActivity extends AppCompatActivity {
 
         menu.add(0, 0, 0, "Website");
         menu.add(0, 1, 1, "Contact the bank");
+        menu.add(0, 2, 2, "Favourite");
 
         if (v == DBS) {
             wordClicked = "DBS";
@@ -56,7 +64,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
@@ -68,13 +75,13 @@ public class MainActivity extends AppCompatActivity {
 
         if (id == R.id.EnglishSelection) {
             DBS.setText("DBS");
-            OCBC.setText("OCBC ");
+            OCBC.setText("OCBC");
             UOB.setText("UOB ");
             return true;
         } else if (id == R.id.ChineseSelection) {
-            DBS.setText("星展银行");
-            OCBC.setText("华侨银行");
-            UOB.setText("大华银行");
+            DBS.setText("DBSchi");
+            OCBC.setText("OCBCchi");
+            UOB.setText("UOBchi");
             return true;
         }
         return super.onOptionsItemSelected(item);
@@ -85,29 +92,55 @@ public class MainActivity extends AppCompatActivity {
         if (wordClicked.equalsIgnoreCase("DBS")) {
             if (item.getItemId() == 0) { //check whether the selected menu item ID is 0
                 Intent intent = new Intent(Intent.ACTION_VIEW,
-                        Uri.parse("https://www.dbs.com/default.page?gclid=CjwKCAjw47eFBhA9EiwAy8kzNFFqrlh5a_DSjHltj56pUQznXHwbzklGcCbcROCj47eTs__jVoKO0xoCfmYQAvD_BwE&gclsrc=aw.ds"));
+                        Uri.parse("@string/DBSlink"));
                 startActivity(intent);
             } else if (item.getItemId() == 1) {
-                Intent intentCall = new Intent(Intent.ACTION_DIAL, Uri.parse("tel:" + "18001111111"));
+                Intent intentCall = new Intent(Intent.ACTION_DIAL, Uri.parse("tel:" + "@string/DBSnum"));
                 startActivity(intentCall);
-            }
-        } else if (wordClicked.equalsIgnoreCase("OCBC")) {
-            if (item.getItemId() == 0) {
-                Intent intent1 = new Intent(Intent.ACTION_VIEW,
-                        Uri.parse("https://www.ocbc.com/group/gateway"));
-                startActivity(intent1);
-            } else if (item.getItemId() == 1) {
-                Intent intentCall1 = new Intent(Intent.ACTION_DIAL, Uri.parse("tel:" + "18003633333"));
-                startActivity(intentCall1);
-            }
-        } else if (wordClicked.equalsIgnoreCase("UOB")) {
-            if (item.getItemId() == 0) {
-                Intent intent2 = new Intent(Intent.ACTION_VIEW,
-                        Uri.parse("https://www.uob.com.sg/personal/index.page"));
-                startActivity(intent2);
-            } else if (item.getItemId() == 1) {
-                Intent intentCall2 = new Intent(Intent.ACTION_DIAL, Uri.parse("tel:" + "18002222121"));
-                startActivity(intentCall2);
+            } else if (item.getItemId() == 2) {
+                if (!FavouriteDBS) {
+                    FavouriteDBS = true;
+                    DBS.setTextColor(Color.RED);
+                } else {
+                    FavouriteDBS = false;
+                    DBS.setTextColor(Color.BLACK);
+                }
+
+            } else if (wordClicked.equalsIgnoreCase("OCBC")) {
+                if (item.getItemId() == 0) {
+                    Intent intent1 = new Intent(Intent.ACTION_VIEW,
+                            Uri.parse("@string/OCBClink"));
+                    startActivity(intent1);
+                } else if (item.getItemId() == 1) {
+                    Intent intentCall1 = new Intent(Intent.ACTION_DIAL, Uri.parse("tel:" + "@string/OCBCnum"));
+                    startActivity(intentCall1);
+                } else if (item.getItemId() == 2) {
+                    if (!FavouriteOCBC) {
+                        FavouriteOCBC = true;
+                        OCBC.setTextColor(Color.RED);
+                    } else {
+                        FavouriteOCBC = false;
+                        OCBC.setTextColor(Color.BLACK);
+                    }
+                } else if (wordClicked.equalsIgnoreCase("UOB")) {
+                    if (item.getItemId() == 0) {
+                        Intent intent2 = new Intent(Intent.ACTION_VIEW,
+                                Uri.parse("@string/UOBlink"));
+                        startActivity(intent2);
+                    } else if (item.getItemId() == 1) {
+                        Intent intentCall2 = new Intent(Intent.ACTION_DIAL, Uri.parse("tel:" + "@string/UOBnum"));
+                        startActivity(intentCall2);
+                    } else if (item.getItemId() == 2) {
+                        if (!FavouriteUOB) {
+                            FavouriteUOB = true;
+                            UOB.setTextColor(Color.RED);
+                        } else {
+                            FavouriteUOB = false;
+                            UOB.setTextColor(Color.BLACK);
+                        }
+                    }
+
+                }
             }
         }
         return super.onContextItemSelected(item); //pass menu item to the superclass implementation
